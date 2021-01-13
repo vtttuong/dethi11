b5662785e354db51af879442722e36efac1fe275
vtttuong <50761736+vtttuong@users.noreply.github.com>
2021-01-13 13:23:19 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="vi">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài t?p tr?c nghi?m toán 11</title>
+</head>
+
+<body>
+    <div>
+        <table style="border:1px solid black">
+            <tr>
+                <th><iframe src="https://drive.google.com/file/d/1dPC3JAgXIz-nLs914kheR76rR0ToAWfR/preview" width="1000" height="600" frameborder="0" marginheight="0" marginwidth="0">?ang t?i?</iframe>
+                </th>
+                <th>
+                    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfvwfmaiiVNOq4I3EoRw0d8D6dNntLXUN_ETPFDsuHgILHYig/viewform" width="360" height="580" frameborder="0" marginheight="0" marginwidth="0">?ang t?i?</iframe>
+                    <span style="text-align:center;color:blue">Giáo viên: Nguy?n Th? Thu </span>
+                </th>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
