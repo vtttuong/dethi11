bacbf7bc2560c3bf3b91c52087ec170ef6e0ad29
vtttuong <50761736+vtttuong@users.noreply.github.com>
2021-01-13 13:10:24 +0700

    indexphp

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+    <title>Bài t?p tr?c nghi?m</title>
+    <meta charset="utf-8">
+</head>
+<body>
+<center>
+    <table border="0">
+        <tr>
+			<th><iframe src="https://drive.google.com/file/d/1dPC3JAgXIz-nLs914kheR76rR0ToAWfR/preview" width="1000" height="600" frameborder="0" marginheight="0" marginwidth="0">Ðang t?i…</iframe>
+			</th>
+            		<th>
+			<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfvwfmaiiVNOq4I3EoRw0d8D6dNntLXUN_ETPFDsuHgILHYig/viewform" width="360" height="580" frameborder="0" marginheight="0" marginwidth="0">Ðang t?i…</iframe>
+			<center><font color="blue">Giáo Viên: Nguy?n Th? Thu </font></center>
+			</th>
+        </tr>
+    </table>
+</center>
+</body>
+</html>
